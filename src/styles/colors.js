c36c61ea5a2f99578136c7e1a1181e0419908d16
katwal-dipak <katwal.dipak@hotmail.com>
2019-08-12 17:32:44 +0530

    basic setup

---
+++ src/styles/colors.js
@@ -0,0 +1,32 @@
+// eslint-disable-next-line no-undef
+export default (colors = {
+  Golden: {
+    core: "#F5AD32",
+    tint75Percent: "#DAA520"
+  },
+  Black: {
+    core: "#000000",
+    tint75Percent: "#141414",
+    tint50Percent: "#404040",
+    tint25Percent: "#808080"
+  },
+  White: {
+    core: "#FFFFFF",
+    tint75Percent: "#F7F5F3",
+    tint50Percent: "#F7F7F7"
+  },
+  Concrete: {
+    core: "#B1B2B4",
+    tint75Percent: "#C5C5C7",
+    tint50Percent: "#D8D9D9",
+    tint25Percent: "#EBECEC"
+  },
+  Orange: {
+    core: "#E95926"
+  },
+  ComponentColors: {
+    imageBackground: "#F7F7F7", // White.tint50Percent
+    borderColor: "#D8D9D9", // Concrete.tint50Percent,
+    semiTransparentDarkOverlay: "rgba(60, 60, 60, 0.5)"
+  }
+});
