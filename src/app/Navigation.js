60e72df0473f2e1804ae953ac652a6e3b1857f86
Dipak Katwal <katwal.dipak@hotmail.com>
2019-09-03 00:39:36 +0530

    set up navigator

---
+++ src/app/Navigation.js
@@ -0,0 +1,61 @@
+import React from "react";
+import {
+  createStackNavigator,
+  createSwitchNavigator,
+  createAppContainer,
+  createBottomTabNavigator
+} from "react-navigation";
+import { StyleSheet } from "react-native";
+
+import Home from "../scene/Home/HomeContainer";
+import Login from "../scene/Login/LoginContainer";
+
+const AppStackNavigator = createStackNavigator({
+  Home: {
+    screen: Home,
+    navigationOptions: props => ({
+      // header: null
+      // https://reactnavigation.org/docs/navigators/stack check this for styles
+    })
+  }
+});
+
+const AuthStack = createStackNavigator({
+  Login: {
+    screen: Login,
+    navigationOptions: () => ({
+      title: "Mobile Verification"
+    })
+  }
+});
+
+const TabNavigator = createBottomTabNavigator(
+  {
+    HOME: AppStackNavigator
+    // EXPLORE: ESStackNavigator,
+    // SELL: ExploreStackNavigator,
+    // PREMIUM: PremiumAdStackNavigator,
+    // SETTINGS: SettingsStackNavigator
+  },
+  {
+    // defaultNavigationOptions: ({ navigation }) => ({}),
+    tabBarOptions: {
+      // activeTintColor: Black.tint75Percent,
+      // inactiveTintColor: Black.tint25Percent,
+      showLabel: false,
+      style: { marginBottom: 5 }
+    }
+  }
+);
+
+export default createAppContainer(
+  createSwitchNavigator(
+    {
+      App: TabNavigator,
+      Auth: AuthStack
+    },
+    {
+      initialRouteName: "App"
+    }
+  )
+);
