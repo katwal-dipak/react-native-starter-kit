9057653d14f92a961e44f6ce66db50c4f6089977
Dipak Katwal <katwal.dipak@hotmail.com>
2019-09-10 23:49:17 +0530

    top property card list view

--- src/scene/Home/Home.js
+++ src/scene/Home/Home.js
@@ -3,7 +3,35 @@ import { StyleSheet, Text, View, FlatList, Image } from "react-native";
 
 import { styles } from "./styles";
 
-const { favouriteCard } = styles;
+const { favouriteCard, topListCard } = styles;
+
+const FavListData = [
+  {
+    name: "dipak",
+    imageURL:
+      "https://static.independent.co.uk/s3fs-public/thumbnails/image/2018/10/01/15/sc1827-571.jpg?w968h681"
+  },
+  {
+    name: "sujal",
+    imageURL:
+      "https://cdn.vox-cdn.com/thumbor/7H-UkG1HYA8_AWAXBEK-Sod5CvM=/0x0:3000x2161/1200x675/filters:focal(1364x1124:1844x1604)/cdn.vox-cdn.com/uploads/chorus_image/image/57382215/Michel_ArnaudRajiv__7.0.jpg"
+  },
+  {
+    name: "sujal",
+    imageURL:
+      "https://cdn.decoist.com/wp-content/uploads/2016/10/Modern-private-apartment-in-Milan-deisgned-by-Cristiana-Vannini.jpg"
+  },
+  {
+    name: "sujal",
+    imageURL:
+      "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQXPYfLDMm8Pla1OKmbe3CDzJNMBv15w8wTnLXcFGMGwH21b8YO"
+  },
+  {
+    name: "sujal",
+    imageURL:
+      "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTjiT7Wrbep-L4hRNAgqsb81hVi5ipqhU_h6fKYbnwXW80jBczVTw"
+  }
+];
 
 class Home extends React.Component {
   keyExtractor = (item, index) => index.toString();
@@ -20,38 +48,24 @@ class Home extends React.Component {
     );
   };
 
+  renderTopListCard = ({ item }) => {
+    return (
+      <View style={topListCard}>
+        <Image
+          style={topListCard}
+          source={{ uri: item.imageURL }}
+          resizeMode="cover"
+        />
+      </View>
+    );
+  };
+
   renderFavouriteList = () => {
     return (
       <View style={{ padding: 15 }}>
         <FlatList
           showsHorizontalScrollIndicator={false}
-          data={[
-            {
-              name: "dipak",
-              imageURL:
-                "https://static.independent.co.uk/s3fs-public/thumbnails/image/2018/10/01/15/sc1827-571.jpg?w968h681"
-            },
-            {
-              name: "sujal",
-              imageURL:
-                "https://cdn.vox-cdn.com/thumbor/7H-UkG1HYA8_AWAXBEK-Sod5CvM=/0x0:3000x2161/1200x675/filters:focal(1364x1124:1844x1604)/cdn.vox-cdn.com/uploads/chorus_image/image/57382215/Michel_ArnaudRajiv__7.0.jpg"
-            },
-            {
-              name: "sujal",
-              imageURL:
-                "https://cdn.decoist.com/wp-content/uploads/2016/10/Modern-private-apartment-in-Milan-deisgned-by-Cristiana-Vannini.jpg"
-            },
-            {
-              name: "sujal",
-              imageURL:
-                "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQXPYfLDMm8Pla1OKmbe3CDzJNMBv15w8wTnLXcFGMGwH21b8YO"
-            },
-            {
-              name: "sujal",
-              imageURL:
-                "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTjiT7Wrbep-L4hRNAgqsb81hVi5ipqhU_h6fKYbnwXW80jBczVTw"
-            }
-          ]}
+          data={FavListData}
           renderItem={this.renderFavouriteCard}
           keyExtractor={this.keyExtractor}
           horizontal
@@ -59,11 +73,28 @@ class Home extends React.Component {
       </View>
     );
   };
+
+  renderTopList = () => {
+    return (
+      <View style={{ padding: 15 }}>
+        <FlatList
+          showsHorizontalScrollIndicator={false}
+          data={FavListData}
+          renderItem={this.renderTopListCard}
+          keyExtractor={this.keyExtractor}
+          horizontal
+        />
+      </View>
+    );
+  };
+
   render() {
     return (
       <Fragment>
         <Text style={{ padding: 15, fontSize: 24 }}>Find Property</Text>
         {this.renderFavouriteList()}
+        <Text style={{ padding: 15, fontSize: 18 }}>Top Property</Text>
+        {this.renderTopList()}
       </Fragment>
     );
   }
--- src/scene/Home/styles.js
+++ src/scene/Home/styles.js
@@ -4,11 +4,19 @@ import { screenWidth } from "../../utilities/ScreenSize";
 
 const favouriteCardWidth = screenWidth * 0.8;
 const favouriteCardHeight = favouriteCardWidth * 0.8;
+const topListCardWidth = screenWidth * 0.4;
+const topListCardHeight = topListCardWidth * 0.8;
 
 export const styles = StyleSheet.create({
   favouriteCard: {
     width: favouriteCardWidth,
     height: favouriteCardHeight,
+    backgroundColor: "#F7F7F7",
+    marginRight: 15
+  },
+  topListCard: {
+    width: topListCardWidth,
+    height: topListCardHeight,
     backgroundColor: "#F7F7F7",
     marginRight: 15
   }
