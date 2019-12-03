cf63a61a7e84097a3724c10e2adde9c09cc03a3d
Dipak Katwal <katwal.dipak@hotmail.com>
2019-12-03 15:21:57 +0530

    :fire: Splash Screen Android

--- android/app/src/main/java/com/yak/MainApplication.java
+++ android/app/src/main/java/com/yak/MainApplication.java
@@ -11,6 +11,7 @@ import com.oblador.vectoricons.VectorIconsPackage;
 import com.facebook.react.ReactNativeHost;
 import com.facebook.react.ReactPackage;
 import com.facebook.soloader.SoLoader;
+import org.devio.rn.splashscreen.SplashScreenReactPackage;
 
 import java.util.List;
 
@@ -27,7 +28,7 @@ public class MainApplication extends Application implements ReactApplication {
       @SuppressWarnings("UnnecessaryLocalVariable")
       List<ReactPackage> packages = new PackageList(this).getPackages();
       // Packages that cannot be autolinked yet can be added manually here, for example:
-      // packages.add(new MyReactNativePackage());
+       packages.add(new SplashScreenReactPackage());
       return packages;
     }
 
