5cf3a82ab1f46c76d8626b9f70e8c9b971841af5
Dipak Katwal <katwal.dipak@hotmail.com>
2019-12-04 10:21:49 +0530

    Minor Changes

--- src/scene/Home/Home.js
+++ src/scene/Home/Home.js
@@ -8,32 +8,32 @@ const { favouriteCard, topListCard } = styles;
 
 const FavListData = [
   {
-    name: "dipak",
+    name: "1",
     imageURL:
       "https://static.independent.co.uk/s3fs-public/thumbnails/image/2018/10/01/15/sc1827-571.jpg?w968h681"
   },
   {
-    name: "sujal",
+    name: "2",
     imageURL:
       "https://cdn.vox-cdn.com/thumbor/7H-UkG1HYA8_AWAXBEK-Sod5CvM=/0x0:3000x2161/1200x675/filters:focal(1364x1124:1844x1604)/cdn.vox-cdn.com/uploads/chorus_image/image/57382215/Michel_ArnaudRajiv__7.0.jpg"
   },
   {
-    name: "sujal",
+    name: "3",
     imageURL:
       "https://cdn.decoist.com/wp-content/uploads/2016/10/Modern-private-apartment-in-Milan-deisgned-by-Cristiana-Vannini.jpg"
   },
   {
-    name: "sujal",
+    name: "4",
     imageURL:
       "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQXPYfLDMm8Pla1OKmbe3CDzJNMBv15w8wTnLXcFGMGwH21b8YO"
   },
   {
-    name: "sujal",
+    name: "5",
     imageURL:
       "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTjiT7Wrbep-L4hRNAgqsb81hVi5ipqhU_h6fKYbnwXW80jBczVTw"
   },
   {
-    name: "sujal",
+    name: "6",
     imageURL:
       "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTjiT7Wrbep-L4hRNAgqsb81hVi5ipqhU_h6fKYbnwXW80jBczVTw"
   }
@@ -101,7 +101,7 @@ class Home extends React.Component {
           style={{
             padding: 15,
             fontSize: 24,
-            fontFamily: Fonts.MonotonRegular
+            fontFamily: Fonts.DidactGothicRegular
           }}
         >
           Find Property
